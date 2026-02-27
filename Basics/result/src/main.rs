d633bf48f2ed4a4fda32ba8b53bbee55b7a08192
NSANTRA <neelotpal.santra@gmail.com>
2026-02-27 19:08:43 +0530

    Modified Result Topic

--- Basics/result/src/main.rs
+++ Basics/result/src/main.rs
@@ -1,9 +1,9 @@
 use std::io::{Write, stdin, stdout};
 use std::num::ParseIntError;
 
-fn multiply(first: &str, second: &str) -> Result<usize, ParseIntError> {
-    match first.trim().parse::<usize>() {
-        Ok(first) => match second.trim().parse::<usize>() {
+fn multiply(first: &str, second: &str) -> Result<isize, ParseIntError> {
+    match first.trim().parse::<isize>() {
+        Ok(first) => match second.trim().parse::<isize>() {
             Ok(second) => Ok(first * second),
             Err(err) => Err(err),
         },
@@ -11,7 +11,7 @@ fn multiply(first: &str, second: &str) -> Result<usize, ParseIntError> {
     }
 }
 
-fn display(result: Result<usize, ParseIntError>) {
+fn display(result: Result<isize, ParseIntError>) {
     match result {
         Ok(product) => {
             println!("The product of the given 2 numbers are: {product}");
@@ -24,9 +24,9 @@ fn display(result: Result<usize, ParseIntError>) {
 
 type ParseResult<T> = Result<T, ParseIntError>;
 
-fn addition(first: &str, second: &str) -> ParseResult<usize> {
-    match first.trim().parse::<usize>() {
-        Ok(first) => match second.trim().parse::<usize>() {
+fn addition(first: &str, second: &str) -> ParseResult<isize> {
+    match first.trim().parse::<isize>() {
+        Ok(first) => match second.trim().parse::<isize>() {
             Ok(second) => Ok(first + second),
             Err(err) => Err(err),
         },
@@ -34,13 +34,23 @@ fn addition(first: &str, second: &str) -> ParseResult<usize> {
     }
 }
 
-fn display_alias(result: ParseResult<usize>) {
+fn subtraction(first: &str, second: &str) -> ParseResult<isize> {
+    match first.trim().parse::<isize>() {
+        Ok(first) => match second.trim().parse::<isize>() {
+            Ok(second) => Ok(first - second),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
+fn display_alias(result: ParseResult<isize>) {
     match result {
         Ok(number) => {
             println!("The addition of the given 2 numbers are: {number}");
         }
         Err(err) => {
-            println!("Error encountered while addition: {err}");
+            println!("Error encountered while operation: {err}");
         }
     }
 }
@@ -60,5 +70,8 @@ fn main() {
     display(result);
 
     let result = addition(&first, &second);
+    display_alias(result);
+    
+    let result = subtraction(&first, &second);
     display_alias(result);
 }
