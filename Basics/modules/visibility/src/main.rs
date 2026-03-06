a55df13a90816d9c805aefe6461388718f1c1dd5
NSANTRA <neelotpal.santra@gmail.com>
2026-03-06 17:46:39 +0530

    Modified Mod Visibility Topic

--- Basics/modules/visibility/src/main.rs
+++ Basics/modules/visibility/src/main.rs
@@ -1,3 +1,19 @@
+mod struct_mod {
+    pub struct OpenBox {
+        pub content: String,
+    }
+
+    pub struct CloseBox {
+        content: String,
+    }
+
+    impl CloseBox {
+        pub fn new(content: String) -> CloseBox {
+            CloseBox { content: content }
+        }
+    }
+}
+
 mod custom_mod {
     fn private_func() {
         println!("Called custom_mod::private_func");
@@ -6,7 +22,7 @@ mod custom_mod {
     pub fn public_func() {
         println!("Called custom_mod::public_func");
     }
-    
+
     pub fn indirect_func() {
         println!("Called custom_mod::indirect_func");
         private_func();
@@ -16,18 +32,18 @@ mod custom_mod {
         pub fn public_func() {
             println!("Called public of custom_mod::nested::public_func");
         }
-        
+
         #[allow(dead_code)]
         fn private_func() {
             println!("Called custom_mod::nested::private_func");
         }
 
-        pub (in crate::custom_mod) fn public_in_custom_mod() {
+        pub(in crate::custom_mod) fn public_in_custom_mod() {
             println!("Called custom_mod::nested::public_in_custom_mod");
         }
-        
+
         // Functions declared using super are only visible within the parent module
-        pub (super) fn public_in_super() {
+        pub(super) fn public_in_super() {
             println!("Called custom_mod::nested::public_in_super");
         }
     }
@@ -38,7 +54,7 @@ mod custom_mod {
         nested::public_in_super();
     }
 
-    pub (crate) fn public_function_in_crate() {
+    pub(crate) fn public_function_in_crate() {
         println!("Called custom_mod::public_function_in_crate");
     }
 
@@ -49,7 +65,7 @@ mod custom_mod {
         }
 
         #[allow(dead_code)]
-        pub (crate) fn restricted_function() {
+        pub(crate) fn restricted_function() {
             println!("Called custom_mod::private_nested::restricted_function");
         }
     }
@@ -71,4 +87,13 @@ fn main() {
     custom_mod::call_public_function_custom_mod();
 
     custom_mod::public_function_in_crate();
+
+    let open_box: struct_mod::OpenBox = struct_mod::OpenBox {
+        content: String::from("Open Box"),
+    };
+
+    println!("Contents of open_box: {}", open_box.content);
+
+    #[allow(unused_variables)]
+    let close_box = struct_mod::CloseBox::new(String::from("Close Box"));
 }
